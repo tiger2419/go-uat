37f2ee07e4e562ccf59059886c7fdc95a46d5330
zz-tiger <ken2423236821@gmail.com>
2024-04-27 16:29:33 +0800

    notes

--- README.md
+++ README.md
@@ -14,8 +14,55 @@ type Cat struct{
 
 
 基本数据类型转string
+方式一:
   var data
   fmt.Sprintf("%d",data)
+方式二:
+  strconv包的format函数
+  strconv.FormatBool()
+  strconv.FormatFloat()
+  strconv.FormatInt()
+  strconv.FormatUint()
+-----------------------------------------
+string转基本数据类型
+base代表进制 2->二进制 ...
+bitSize代表整型类型 有0 8 16 32 64 分别对应 int int8 int16 int32 int64
+这里bitSize没有 4 24
+strconv.ParseInt(s string, base int, bitSize int)
+strconv.ParseUint()
+strconv.ParseFloat()
+strconv.ParseInt()
+strconv.Atoi()  //默认十进制
+--------------------------------------
+获取终端的方式
+调用 fmt.Scanln(),例如下
+  var scanln string
+  fmt.Scanln(&scanln)
 
+调用fmt.Scanf(),一般用于一次性输入多个
+var name string
+var age int
+var salary float64
+fmt.Scanf("%s %d %f",&name,&age,&salary)
+==========================================================
+位运算规则:
+正数,先转换成补码,用补码运算  
+  - 右移, 低位移除后,缺位的高位用符号位的值填补,因为正数的符号是0,所以用0来补充缺位
+  - 左移, 符号位不变,低位缺位用0补充
+负数,先转补码再用补码运算
+  - 右移, 低位移除后,缺位的高位用符号位的值填补,因为负数的符号是1,所以用1来补充高位的缺位
+  - 左移, 符号位不变,低位缺位用0补充
+上面的正负数左移规则是针对未高位溢出的情况。
+当高位溢出时符号位也参与运算,例如
+  - var i int8 = 5 --> 00000101
+  上面的i最多支持8位,当第一个1移到最左端时,就是一个负数了,但仍然是一个补码;第二个1都移走时,i就永远是0了
+==========================================================
+
+
+
+
+
+
+==========================================================
 
 
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sort"
+	"strconv"
 	"sync"
 	"uat/entity"
 )
@@ -14,7 +16,76 @@ var group = sync.WaitGroup{}
 var index = 10
 var channel chan int = make(chan int)
 
+func codeTest() {
+
+	var index int8 = -1
+
+	for i := 1; ; i++ {
+
+		val := index >> i
+
+		fmt.Println(val)
+
+		if i == 5 {
+			goto label
+		}
+	}
+
+label:
+	{
+		fmt.Println("")
+	}
+
+}
+
+type student struct {
+	age  int
+	name string
+}
+
+type stuSlice []student
+
+func (s stuSlice) Len() int {
+	return len(s)
+}
+func (s stuSlice) Less(i, j int) bool {
+	return s[i].age > s[j].age
+}
+
+func (s stuSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func init() {
+	fmt.Println("init......")
+	fmt.Println("开启初始化工作......")
+
+	var students = stuSlice{
+		{age: 20, name: "tanxiaomi"},
+		{age: 22, name: "tandami"},
+		{age: 23, name: "tanmi"},
+	}
+	fmt.Println(students)
+	fmt.Println(&students)
+	sort.Sort(students)
+	fmt.Println(students)
+	fmt.Println(&students)
+}
+
 func main() {
+
+	codeTest()
+
+	var a int = 645
+	var ptr *int = &a
+	var scanln string
+	fmt.Scanln(&scanln)
+	fmt.Println("输入的内容是: ", scanln)
+
+	fmt.Scanf("", &scanln)
+	fmt.Println("格式化后的内容: ", scanln)
+
+	fmt.Println(ptr)
+	fmt.Println(*ptr)
 
 	fmt.Println("hello world")
 
@@ -22,8 +93,14 @@ func main() {
 
 	//group.Add(10)
 
-	var strInt int = 64
+	var num1 string = "65"
+	fmt.Println(strconv.ParseInt(num1, 10, 0))
+	//strconv.Itoa()
 
+	// base 代表数值进制 10->十进制 2->二进制
+	formatUint := strconv.FormatUint(9981, 10)
+	fmt.Println(formatUint)
+	var strInt int = 64
 	sprintln := fmt.Sprintln(strInt)
 	fmt.Println(sprintln)
 	for i := 1; i <= 10; i++ {
